f8e831741a1b5ed187a514dcd8567968f8345a66
criseida0310 <89651243+criseida0310@users.noreply.github.com>
2025-12-29 13:04:24 -0600

    feat: align Client entity with gym churn database schema

--- src/main/java/com/example/demo/domain/client/Client.java
+++ src/main/java/com/example/demo/domain/client/Client.java
@@ -1,7 +1,7 @@
 package com.example.demo.domain.client;
 
 import jakarta.persistence.*;
-import java.time.LocalDate;
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "clients")
@@ -11,31 +11,149 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String plan;
+    @Column(name = "client_name")
+    private String clientName;
 
-    @Column(name = "fecha_alta")
-    private LocalDate fechaAlta;
+    private String gender;
 
-    public Client() {}
+    @Column(name = "near_location")
+    private Integer nearLocation;
+
+    @Column(name = "partner_employee")
+    private Integer partnerEmployee;
+
+    @Column(name = "promo_friends")
+    private Integer promoFriends;
+
+    @Column(name = "client_phone")
+    private String clientPhone;
+
+    private Integer age;
+
+    @Column(name = "contract_period")
+    private Integer contractPeriod;
+
+    @Column(name = "month_to_end_contract")
+    private Integer monthToEndContract;
+
+    @Column(name = "lifetime_months")
+    private Integer lifetimeMonths;
+
+    @Column(name = "avg_class_frequency_total")
+    private BigDecimal avgClassFrequencyTotal;
+
+    @Column(name = "avg_class_frequency_current_month")
+    private BigDecimal avgClassFrequencyCurrentMonth;
+
+    private Integer churn;
+
+    // ===== Getters y Setters =====
 
     public Long getId() {
         return id;
     }
 
-    public String getPlan() {
-        return plan;
+    public String getClientName() {
+        return clientName;
     }
 
-    public void setPlan(String plan) {
-        this.plan = plan;
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
     }
 
-    public LocalDate getFechaAlta() {
-        return fechaAlta;
+    public String getGender() {
+        return gender;
     }
 
-    public void setFechaAlta(LocalDate fechaAlta) {
-        this.fechaAlta = fechaAlta;
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getNearLocation() {
+        return nearLocation;
+    }
+
+    public void setNearLocation(Integer nearLocation) {
+        this.nearLocation = nearLocation;
+    }
+
+    public Integer getPartnerEmployee() {
+        return partnerEmployee;
+    }
+
+    public void setPartnerEmployee(Integer partnerEmployee) {
+        this.partnerEmployee = partnerEmployee;
+    }
+
+    public Integer getPromoFriends() {
+        return promoFriends;
+    }
+
+    public void setPromoFriends(Integer promoFriends) {
+        this.promoFriends = promoFriends;
+    }
+
+    public String getClientPhone() {
+        return clientPhone;
+    }
+
+    public void setClientPhone(String clientPhone) {
+        this.clientPhone = clientPhone;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getContractPeriod() {
+        return contractPeriod;
+    }
+
+    public void setContractPeriod(Integer contractPeriod) {
+        this.contractPeriod = contractPeriod;
+    }
+
+    public Integer getMonthToEndContract() {
+        return monthToEndContract;
+    }
+
+    public void setMonthToEndContract(Integer monthToEndContract) {
+        this.monthToEndContract = monthToEndContract;
+    }
+
+    public Integer getLifetimeMonths() {
+        return lifetimeMonths;
+    }
+
+    public void setLifetimeMonths(Integer lifetimeMonths) {
+        this.lifetimeMonths = lifetimeMonths;
+    }
+
+    public BigDecimal getAvgClassFrequencyTotal() {
+        return avgClassFrequencyTotal;
+    }
+
+    public void setAvgClassFrequencyTotal(BigDecimal avgClassFrequencyTotal) {
+        this.avgClassFrequencyTotal = avgClassFrequencyTotal;
+    }
+
+    public BigDecimal getAvgClassFrequencyCurrentMonth() {
+        return avgClassFrequencyCurrentMonth;
+    }
+
+    public void setAvgClassFrequencyCurrentMonth(BigDecimal avgClassFrequencyCurrentMonth) {
+        this.avgClassFrequencyCurrentMonth = avgClassFrequencyCurrentMonth;
+    }
+
+    public Integer getChurn() {
+        return churn;
+    }
+
+    public void setChurn(Integer churn) {
+        this.churn = churn;
     }
 }
